fc523afde290de291b1eebf029cea1d5aa818881
“teji1998” <tejasvinirpk@gmail.com>
2020-12-16 20:02:14 +0530

    Added:Code to establish a connection between database and ADO dotnet

--- EmployeePayrollService/EmployeeRepository.cs
+++ EmployeePayrollService/EmployeeRepository.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Text;
 
 namespace EmployeePayrollService
 {
     class EmployeeRepository
     {
+        public static string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Employee_Payroll_Service;Integrated Security=True";
+        SqlConnection connection = new SqlConnection(connectionString);
     }
 }
