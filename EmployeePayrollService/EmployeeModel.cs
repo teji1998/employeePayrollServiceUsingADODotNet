a6ef0bfd161653cb2f70c0c6597ee014fa0068fb
“teji1998” <tejasvinirpk@gmail.com>
2020-12-16 20:24:12 +0530

    Added:POCO class for employee payroll

--- EmployeePayrollService/EmployeeModel.cs
+++ EmployeePayrollService/EmployeeModel.cs
@@ -6,5 +6,18 @@ namespace EmployeePayrollService
 {
     class EmployeeModel
     {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double Basic_Pay { get; set; }
+        public string Start_Date { get; set; }
+        public string Gender { get; set; }
+        public string Mobile_number { get; set; }
+        public string Address { get; set; }
+        public string Department { get; set; }
+        public double Deductions { get; set; }
+        public double Taxable_Pay { get; set; }
+        public double Income_Tax { get; set; }
+        public double Net_Pay { get; set; }
     }
 }
+
